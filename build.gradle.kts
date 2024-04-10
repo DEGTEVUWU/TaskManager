f2f5d63c04d2ffc7de8c68cd7e37e7cfc255c5ad
ivan <diogteff.ivan@yandex.ru>
2024-04-10 13:58:49 +0300

    1

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	application // Плагин для создания исполняемого JAR
+	id("org.springframework.boot") version "3.2.5-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+//application {
+//	mainClass.set("hexlet.code.AppApplication")
+//}
+//
+
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
