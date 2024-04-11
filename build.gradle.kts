5298e92eca5b285ce70e1e2d0041c211cb191ac7
ivan <diogteff.ivan@yandex.ru>
2024-04-11 17:26:14 +0300

    добавил правильные конфиги ломбока

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,16 +24,56 @@ repositories {
 }
 
 dependencies {
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
+
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-validation") //нужна для разного рода валидаций полей сущностей
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("com.h2database:h2")
 
-	compileOnly("org.projectlombok:lombok:1.18.24")
 
+	testCompileOnly("org.projectlombok:lombok:1.18.30")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+//	implementation("org.springframework.boot:spring-boot-starter")
+//	implementation("org.springframework.boot:spring-boot-starter-web")
+//	developmentOnly("org.springframework.boot:spring-boot-devtools")
+//
+//	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+//	implementation("org.springframework.boot:spring-boot-starter-validation")
+//
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+//
+//	compileOnly("org.projectlombok:lombok")
+//	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+//
+//	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+//	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+//
+//	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+//
+//	runtimeOnly("com.h2database:h2")
+//	runtimeOnly("org.postgresql:postgresql")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+//	testImplementation("org.springframework.security:spring-security-test")
+//	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+//	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+//	implementation("net.datafaker:datafaker:2.0.1")
+//	implementation("org.instancio:instancio-junit:3.3.0")
+//
+//	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
 
 tasks.withType<Test> {
--- src/test/java/hexlet/code/app/model/User.java
+++ src/test/java/hexlet/code/app/model/User.java
@@ -1,6 +1,12 @@
 package hexlet.code.app.model;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jdk.jfr.SettingDefinition;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -8,6 +14,8 @@ import java.time.LocalDate;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
@@ -15,15 +23,18 @@ public class User {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    // @NotBlank
+    @Column(nullable = true)
     private String firstName;
 
-    // @NotBlank
+    @Column(nullable = true)
     private String lastName;
     // EMAIL
     @Column(unique = true)
+    @Email
     private String email;
 
+    @Column(nullable = false)
+    @Size(min = 3)
     private String password;
 
     @LastModifiedDate
@@ -33,61 +44,61 @@ public class User {
     private LocalDate createdAt;
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDate getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDate updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public LocalDate getUpdatedAt() {
+//        return updatedAt;
+//    }
+//
+//    public void setUpdatedAt(LocalDate updatedAt) {
+//        this.updatedAt = updatedAt;
+//    }
+//
+//    public LocalDate getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDate createdAt) {
+//        this.createdAt = createdAt;
+//    }
 }
 
 
