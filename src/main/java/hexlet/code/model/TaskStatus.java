046fcc5bb70bf23d0ddf8505d60f265e0c310ef8
ivan <diogteff.ivan@yandex.ru>
2024-04-15 13:31:50 +0300

    добавил сущность таск-статусов

---
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -0,0 +1,74 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.IdGeneratorType;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@Table(name = "task_status")
+@EntityListeners(AuditingEntityListener.class)
+public class TaskStatus implements BaseEntity, UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(min = 1)
+    private String name;
+
+    @Column(nullable = false)
+    @Size(min = 1)
+    private String slug;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
