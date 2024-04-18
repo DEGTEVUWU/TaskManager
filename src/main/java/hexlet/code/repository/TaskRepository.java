d25ee90e7a65bc20b418f245ac30da9334dd6630
ivan <diogteff.ivan@yandex.ru>
2024-04-18 17:39:54 +0300

    добавил класс спецификации для определения 4 параметров при запросе для вывода тасок

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -5,13 +5,14 @@ import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import hexlet.code.model.Task;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     Optional<Task> findByDescription(String slug);
     List<Task> findByAssignee(User user);
     List<Task> findByTaskStatus(TaskStatus taskStatus);
