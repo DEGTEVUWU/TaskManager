bcc1c1d384355472ee741a5034647f84ca9c1a28
ivan <diogteff.ivan@yandex.ru>
2024-04-27 10:29:29 +0300

    еще раз проверил линтер

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -1,15 +1,8 @@
 package hexlet.code.repository;
-
-import hexlet.code.model.Label;
-import hexlet.code.model.TaskStatus;
-import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import hexlet.code.model.Task;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
