68c9b99d5a4043571267b46f719c7b659fae50a3
ivan <diogteff.ivan@yandex.ru>
2024-04-30 16:48:06 +0300

    переопределил методы икуалс и хеш-код в сущности таск

--- src/main/java/hexlet/code/model/Task.java
+++ src/main/java/hexlet/code/model/Task.java
@@ -19,6 +19,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -53,4 +54,18 @@ public class Task implements BaseEntity {
 
     @CreatedDate
     private LocalDate createdAt;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        return Objects.equals(id, other.id);
+    }
 }
