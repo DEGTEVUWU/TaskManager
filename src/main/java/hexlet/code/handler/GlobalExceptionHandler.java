11e51b2ac74ed639d1b815b3b481ab043c338ca9
ivan <diogteff.ivan@yandex.ru>
2024-04-30 16:15:47 +0300

    добавил обработчик для простого Exeption

--- src/main/java/hexlet/code/handler/GlobalExceptionHandler.java
+++ src/main/java/hexlet/code/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package hexlet.code.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         return ResponseEntity.badRequest().body("Invalid request data");
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
     }
 }
